b34f51d50943bcf18bfde7fed0593fd8dae09391
Dave Amison <dave.amison@sixstar.global>
2023-10-01 08:36:27 +0000

    basic function structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [numberToGuess, setNumberToGuess] = useState(null);
+  const [guess, setGuess] = useState(null);
+
+  useEffect(() => {
+    const number = Math.floor(Math.random() * 100);
+    setNumberToGuess(number);
+  }, []);
+
+  const handleGuessChange = (event) => {
+    setGuess(Number(event.target.value));
+  };
+
+  const checkNumber = () => {
+    console.log(guess)
+    if (!guess) {
+      alert('No Number Selected');
+      return;
+    }
+    if (guess === numberToGuess) {
+      alert('You won!');
+    } else if (guess > numberToGuess) {
+      alert('Too high');
+    } else {
+      alert('Too low');
+    }
+  }
+
+
   return (
     <div className="App">
-      Guess The Number
+      <h1>Guess the number</h1>
+      <input
+        type="number"
+        id="numberInput"
+        value={guess}
+        onChange={handleGuessChange}
+      />
+      <button onClick={checkNumber}>Check</button>
     </div>
   );
 }
